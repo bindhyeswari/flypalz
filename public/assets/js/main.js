fc0d5c5c34e57f6aa51c011aa8ab46e6963aa21e
Bindhyeswari Prasad Mishra <bmishra@netflix.com>
2017-10-20 17:20:30 -0700

    first draft

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -25,59 +25,6 @@
         });
 
         // revolution slider start
-        $("#rev_slider_1").show().revolution({
-            sliderType: "standard",
-            sliderLayout: "fullwidth",
-            dottedOverlay: "none",
-            delay: 9000,
-            spinner: "spinner4",
-            navigation: {
-                keyboardNavigation: "off",
-                keyboard_direction: "horizontal",
-                mouseScrollNavigation: "off",
-                onHoverStop: "on",
-                touch: {
-                    touchenabled: "on",
-                    swipe_threshold: 75,
-                    swipe_min_touches: 1,
-                    swipe_direction: "horizontal",
-                    drag_block_vertical: false
-                },
-                arrows: {
-                    enable: true,
-                    style: 'metis',
-                    tmp: '',
-                    rtl: false,
-                    hide_onleave: true,
-                    hide_onmobile: true,
-                    hide_under: 0,
-                    hide_over: 9999,
-                    hide_delay: 200,
-                    hide_delay_mobile: 1200,
-                    left: {
-                        container: 'slider',
-                        h_align: 'left',
-                        v_align: 'center',
-                        h_offset: 20,
-                        v_offset: 0
-                    },
-                    right: {
-                        container: 'slider',
-                        h_align: 'right',
-                        v_align: 'center',
-                        h_offset: 20,
-                        v_offset: 0
-                    }
-                },
-            },
-            responsiveLevels: [1240, 1024, 767, 480],
-            gridwidth: [1170, 1170, 767, 480],
-            gridheight:[768,768,960,720],
-            lazyType: "none",
-            shadow: 0,
-            shuffle: "off",
-            autoHeight: "off",
-        });
         // revolution slider end
 
 
